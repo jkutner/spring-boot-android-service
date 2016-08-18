967e384452069e210bff8d3be2c0aa645e132929
Joe Kutner <jpkutner@gmail.com>
2016-08-18 10:49:48 -0500

    Added proper imports for a web application

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @Controller
 @SpringBootApplication
