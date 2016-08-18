a05f614d45c0f907c056be8935af3606ef3c055f
Joe Kutner <jpkutner@gmail.com>
2016-08-18 10:48:36 -0500

    first

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+  @RequestMapping("/")
+  @ResponseBody
+  String home() {
+    return "Hello! This is a demo application linked to this tutorial: ";
+  }
+
+  @RequestMapping("/hello")
+  @ResponseBody
+  String hello() {
+    return "Hello from Heroku!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+}
