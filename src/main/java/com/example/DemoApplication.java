14b3634b2b4d8281c4aa59990acd4ca6f53eae78
Joe Kutner <jpkutner@gmail.com>
2016-08-19 12:36:41 -0500

    added a link in default route

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
   @RequestMapping("/")
   @ResponseBody
   String home() {
-    return "Hello! This is a demo application linked to this tutorial: ";
+    return "Hello! This is a demo application linked to this tutorial: http://jkutner.github.io/2016/08/18/android-backend-api-heroku-retrofit.html";
   }
 
   @RequestMapping("/hello")
